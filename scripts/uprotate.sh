410eb46b9fa8aa49645445bce9b8a21a4bdc2d2e
Bxaifppcts <jacoluccia46@gmail.com>
2022-01-31 23:01:32 -0600

    Delete scripts directory

--- scripts/uprotate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-xrandr -o normal
-xinput set-prop "ELAN9004:00 04F3:2CC4" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
-xinput --enable 13
-xinput --enable 14
-killall onboard
-
