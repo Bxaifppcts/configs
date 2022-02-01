83eafd8a3a5ab43c89ca877dd1350768db3fcfc1
Bxaifppcts <Rusted-Nuts@github.com>
2022-01-31 22:31:37 -0600

    updated more install.sh (dont use, still wip)

--- install.sh
+++ install.sh
@@ -9,7 +9,23 @@ mkdir ~/.scripts
 
 cp -r .config/* ~/.config
 cp -r .scripts/* ~/.scripts
+cp .gtkrc-2.0 ~
 cp .bashrc ~
 cp .alacritty.yml ~
 
-#that's literally it
+#install required programs with yay
+
+#yay -Syu dunst easystroke fish i3-gaps i3blocks picom-jonaburg vimix-gtk-themes alacritty rofi pcmanfm htop discord brave-bin flameshot vim neofetch starship volumeicon onboard networkmanager nm-connection-editor nm-applet xfce4-power-manager lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxsession lxappearance blueman-applet noto-fonts-emoji nitrogen xorg-server pulseaudio pulseaudio-alsa pulseaudio-bluetooth
+
+#enable lightdm
+
+sudo systemctl enable lightdm
+
+#Create a backgrounds folder if you dont have one already
+
+sudo mkdir /usr/share/backgrounds
+sudo cp backgrounds/* /usr/share/backgrounds
+
+#copy lightdm files (requires root)
+
+
