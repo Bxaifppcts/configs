f2811f578011fe5d9c30dc6d314d22c98f9e1187
Bxaifppcts <Rusted-Nuts@github.com>
2022-01-31 20:20:34 -0600

    add fish aliases, remove i3status

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+#Create aliases
+
+#pacman aliases
+alias pu="sudo pacman -Syu"
+alias ps="sudo pacman -S"
+alias pq="sudo pacman -Qq"
+alias pr="sudo pacman -Rns"
+
+#Run starship (keep this one last in order)
 starship init fish | source
 end
