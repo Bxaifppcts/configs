6fd95faac6be66a3af89576647c40f434e4288ec
Bxaifppcts <Rusted-Nuts@github.com>
2022-01-31 20:40:54 -0600

    added .scripts folder installation

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 #creates a config folder if you dont have one already
 
 mkdir ~/.config
+mkdir ~/.scripts
 
 #copies the config files to your home folder
 
 cp -r .config/* ~/.config
+cp -r .scripts/* ~/.scripts
 cp .bashrc ~
 cp .alacritty.yml ~
 
