edeb9ea77d3da23adfdc2ef8eb535203a46500bf
Bxaifppcts <jacoluccia46@gmail.com>
2022-05-14 18:01:33 -0500

    install configs with a smaller font

---
+++ smallerfonts-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# change alacritty font from 13 to 11
+sed -i "s/size: 13/size: 11/g" .alacritty.yml >> .alacritty.yml
+
+# change GTK font from 13 to 10
+sed -i "s/Cantarell 13/Cantarell 11/g" .gtkrc-2.0 >> .gtkrc-2.0
+
+# change i3 size from 11 to 8
+cd .config/i3
+sed -i "s/URWGothic-Book 11/URWGothic-Book 8/g" config >> config
+cd ..
+cd ..
+
+# run the main installer
+sh install.sh
